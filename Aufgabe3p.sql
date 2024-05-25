bd22d515b258ba6252157422da884d2d586d68d0
nyenace <codewithnathan@gmail.com>
2024-05-25 23:43:02 +0200

    databasetabel

---
+++ Aufgabe3p.sql
@@ -0,0 +1,289 @@
+INSERT INTO Element(ElementName)
+VALUES ("Feuer");
+INSERT INTO Element(ElementName)
+VALUES ("Wasser");
+INSERT INTO Element(ElementName)
+VALUES ("Luft");
+INSERT INTO Element(ElementName)
+VALUES ("Erde");
+
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (14,"Feuerfontänenstadt",9000,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (13,"Sichelinsel",3200,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (11,"Ascheninsel",2100,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (10,"Königliche Kraterstadt",1000200,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (9,"Ba Sing Se",202000,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (8,"Omashu",563130,"Erde");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (7,"Wald",50,"Erde");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (6,"Nördlicher Wasserstamm",570,"Wasser");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (5,"Südliche Wasserstamm",5330,"Wasser");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (1,"Nördliche Lufttempel",5120,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (2,"Westliche Lufttempel",2130,"Luft");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (3,"Östliche Lufttempel",43120,"Luft");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (4,"Südliche Lufttempel",21420,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (15,"Nordpol",2230,"Wasser");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (16,"Südpol",2230,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (17,"Zaofu",5021,"Erde");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (18,"Wolfsbucht",2420,"Wasser");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (19,"Hauptstadtinsel",57920,"Feuer");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (20,"Republica",98620,"Erde");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (21,"Kyoshi",490,"Erde");
+INSERT INTO Ort(OrtID, Name, Größe, DominierendeNation) 
+VALUES (22,"Northern Water Tribe",49340,"Wasser");
+
+
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (1, "Zuko", true, 10, 9);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (2, "Aang", true, 4, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (3, "Iroh", true, 10, 9);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (4, "Toph Bei Fong", true, 17, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (5, "Azula", true, 10, 11);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (6, "Katara", true, 18, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (7, "Sokka", true, 18, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (8, "Ozai", true, 10, 10);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (9, "Yue", false, 5, 5);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (10, "Hakoda", false, 14, 14);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (11, "Kuruk", false, 6, 6);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (12, "Roku", false, 10, 10);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (13, "Kyoshi", false, 21, 21);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (14, "Bumi", true, 8, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (15, "Mai", true, 14, 10);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (16, "Ty Lee", true, 10, 11);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (17, "Korra", true, 5, 5);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (18, "Yangchen", false, 2, 2);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (19, "Hei Bai", true, 7, 7);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (20, "Koh", true, 20, 7);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (21, "Appa", true, 4, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (22, "Momo", true, 1, 8);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (23, "Gyatso", false, 4, 4);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (24, "Löwenschildkröte", false, 16, 16);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (25, "Tashi", false, 3, 4);
+INSERT INTO Wesen (WesenID, Name, Alive, Heimat, IstIn) 
+VALUES (26, "Pathik", false, 2, 3);
+
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (8, 1);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (8, 5);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (10, 7);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (10, 6);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (20, 15);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (3, 15);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (20, 7);
+INSERT INTO Familie (ElternTeil, Kind)
+VALUES (23, 2);
+
+INSERT INTO Bender(BenderID, Element)
+VALUES (1, "Feuer");
+INSERT INTO Bender(BenderID, Element)
+VALUES (2, "Luft");
+INSERT INTO Bender(BenderID, Element)
+VALUES (3, "Feuer");
+INSERT INTO Bender(BenderID, Element)
+VALUES (4, "Erde");
+INSERT INTO Bender(BenderID, Element)
+VALUES (5, "Feuer");
+INSERT INTO Bender(BenderID, Element)
+VALUES (6, "Wasser");
+INSERT INTO Bender(BenderID, Element)
+VALUES (8, "Feuer");
+INSERT INTO Bender(BenderID, Element)
+VALUES (9, "Wasser");
+INSERT INTO Bender(BenderID, Element)
+VALUES (14, "Erde");
+INSERT INTO Bender(BenderID, Element)
+VALUES (17, "Wasser");
+INSERT INTO Bender(BenderID, Element)
+VALUES (12, "Feuer");
+INSERT INTO Bender(BenderID, Element)
+VALUES (13, "Erde");
+INSERT INTO Bender(BenderID, Element)
+VALUES (11, "Wasser");
+INSERT INTO Bender(BenderID, Element)
+VALUES (18, "Luft");
+INSERT INTO Bender(BenderID, Element)
+VALUES (23, "Luft");
+INSERT INTO Bender(BenderID, Element)
+VALUES (25, "Luft");
+INSERT INTO Bender(BenderID, Element)
+VALUES (26, "Luft");
+
+INSERT INTO Avatar(AvatarID, Element1, Element2, Element3, Element4, Vorgänger)
+VALUES (18, "Luft", "Wasser", "Erde", "Feuer", 18);
+INSERT INTO Avatar(AvatarID, Element1, Element2, Element3, Element4, Vorgänger)
+VALUES (11, "Wasser", "Erde", "Feuer", "Luft", 18);
+INSERT INTO Avatar(AvatarID, Element1, Element2, Element3, Element4, Vorgänger)
+VALUES (13, "Erde", "Feuer", "Luft", "Wasser", 11);
+INSERT INTO Avatar(AvatarID, Element1, Element2, Element3, Element4, Vorgänger)
+VALUES (12, "Feuer", "Luft", "Wasser", "Erde", 13);
+INSERT INTO Avatar(AvatarID, Element1, Element2, Element3, Element4, Vorgänger)
+VALUES (2, "Luft", "Wasser", "Erde", "Feuer", 12);
+INSERT INTO Avatar(AvatarID, Element1, Element2, Element3, Element4, Vorgänger)
+VALUES (17, "Wasser", "Erde", "Feuer", "Luft", 2);
+
+INSERT INTO Mensch( MenschID, Status, Geld)
+VALUES (15, "Zukos Freundin", 2675);
+INSERT INTO Mensch( MenschID, Status, Geld)
+VALUES (16, "Zirkusartistin",2335);
+INSERT INTO Mensch( MenschID, Status, Geld)
+VALUES (7, "Krieger",3455);
+INSERT INTO Mensch( MenschID, Status, Geld)
+VALUES (10, "Krieger",463);
+
+INSERT INTO Tier( TierID, Rasse)
+VALUES (19, "Bär");
+INSERT INTO Tier( TierID, Rasse)
+VALUES (20, "Tausendfüßlers");
+INSERT INTO Tier( TierID, Rasse)
+VALUES (21, "Himmelsbison");
+INSERT INTO Tier( TierID, Rasse)
+VALUES (22, "Lemur");
+INSERT INTO Tier( TierID, Rasse)
+VALUES (24, "Schildkröte");
+
+INSERT INTO Spirit( SpiritID, Power)
+VALUES (19, 53022);
+INSERT INTO Spirit( SpiritID, Power)
+VALUES (20, 467278);
+INSERT INTO Spirit( SpiritID, Power)
+VALUES (24, 9768656);
+INSERT INTO Spirit( SpiritID, Power)
+VALUES (22, 656);
+
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (10, 8);
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (8, 14);
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (5, 9);
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (7, 19);
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (16, 24);
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (1, 23);
+INSERT INTO Stadt( StadtID, Oberhaupt )
+VALUES (15, 7);
+
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (1, "Luftnation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (2, "Luftnation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (3, "Luftnation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (4, "Luftnation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (5, "Wassernation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (6, "Wassernation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (7, "Erdnation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (8, "Erdnation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (10, "Feuernation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (14, "Feuernation");
+INSERT INTO Gegend( GegendID, Biom )
+VALUES (11, "Feuernation");
+
+INSERT INTO GeisterWeltOrt(GeisterWeltOrtID, Energiekonzentration)
+VALUES (7, 12467);
+INSERT INTO GeisterWeltOrt(GeisterWeltOrtID, Energiekonzentration)
+VALUES (16, 85467);
+INSERT INTO GeisterWeltOrt(GeisterWeltOrtID, Energiekonzentration)
+VALUES (15, 34567);
+INSERT INTO GeisterWeltOrt(GeisterWeltOrtID, Energiekonzentration)
+VALUES (20, 5780323);
+INSERT INTO GeisterWeltOrt(GeisterWeltOrtID, Energiekonzentration)
+VALUES (21, 323);
+
+INSERT INTO Übergang(Von, Nach)
+VALUES (7, 8);
+INSERT INTO Übergang(Von, Nach)
+VALUES (6, 5);
+INSERT INTO Übergang(Von, Nach)
+VALUES (5, 1);
+INSERT INTO Übergang(Von, Nach)
+VALUES (18, 5);
+INSERT INTO Übergang(Von, Nach)
+VALUES (1, 2);
+INSERT INTO Übergang(Von, Nach)
+VALUES (2, 3);
+INSERT INTO Übergang(Von, Nach)
+VALUES (10, 11);
+INSERT INTO Übergang(Von, Nach)
+VALUES (10, 13);
+INSERT INTO Übergang(Von, Nach)
+VALUES (9, 8);
+INSERT INTO Übergang(Von, Nach)
+VALUES (16, 5);
+INSERT INTO Übergang(Von, Nach)
+VALUES (6, 1);
+INSERT INTO Übergang(Von, Nach)
+VALUES (18, 6);
+INSERT INTO Übergang(Von, Nach)
+VALUES (4, 2);
+INSERT INTO Übergang(Von, Nach)
+VALUES (18, 3);
+INSERT INTO Übergang(Von, Nach)
+VALUES (9, 7);
+INSERT INTO Übergang(Von, Nach)
+VALUES (16, 6);
+
+
+
+
+
+
+
+
